f75e6205205dba61d25a86dee061f1dda506f9df
Emanuel <emanuelnaval07@gmail.com>
2024-03-15 12:40:21 -0500

    modify - Finish of 4th form state and modify the html structure of form 2 with radio button

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-const nextBtn = document.getElementById("next-btn");
-const prevBtn = document.getElementById("prev-btn");
+// const nextBtn = document.getElementById("next-btn");
+// const prevBtn = document.getElementById("prev-btn");
 
-const form1 = document.querySelector(".element-1");
-const form2 = document.querySelector(".element-2");
+// const form1 = document.querySelector(".element-1");
+// const form2 = document.querySelector(".element-2");
 
-nextBtn.onclick = function () {
-  form1.classList.remove("displayed-form");
-  form1.classList.add("completed-form");
+// nextBtn.onclick = function () {
+//   form1.classList.remove("displayed-form");
+//   form1.classList.add("completed-form");
 
-  form2.classList.remove("waiting-form");
-  form2.classList.add("displayed-form");
-};
+//   form2.classList.remove("waiting-form");
+//   form2.classList.add("displayed-form");
+// };
 
-prevBtn.onclick = function () {
-  form2.classList.remove("displayed-form");
-  form2.classList.add("waiting-form");
+// prevBtn.onclick = function () {
+//   form2.classList.remove("displayed-form");
+//   form2.classList.add("waiting-form");
 
-  form1.classList.remove("completed-form");
-  form1.classList.add("displayed-form");
-};
+//   form1.classList.remove("completed-form");
+//   form1.classList.add("displayed-form");
+// };
 
 // FORM 1
 
@@ -27,7 +27,7 @@ prevBtn.onclick = function () {
 // - Form button function
 // - change form state and function for previous form
 
-// FORM 2 
+// FORM 2
 // - Function for select only one plan at a time
 //    - Make sure nothing changes if the user re selects a plan
 // - Function that changes the time lapse plan
