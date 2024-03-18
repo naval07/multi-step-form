45361fd8ca0fe603de5dad985e4450f3adcc776b
Emanuel <emanuelnaval07@gmail.com>
2024-03-18 12:54:10 -0500

    Add - Adding the function for radio buttons on form 2

--- script.js
+++ script.js
@@ -33,3 +33,26 @@
 // - Function that changes the time lapse plan
 // - Form buttons functions
 // - change form state and else
+
+// Function that changes the layout of the selected label
+document.addEventListener("DOMContentLoaded", function () {
+  // select radio buttons
+  const radioButtons = document.querySelectorAll(
+    'input[type=radio][name="plan-item-radio"]'
+  );
+  // Event listener for each radio button
+  radioButtons.forEach((elem) => {
+    elem.addEventListener("change", (event) => {
+      // Delete previous selected item layout
+      const labels = document.querySelectorAll("label.plan-item");
+      labels.forEach((l) => {
+        l.classList.remove("selected-item");
+      });
+      // Adding the selected item layout to the new selected item
+      const labelParent = event.target.parentElement;
+      if (event.target.checked) {
+        labelParent.classList.add("selected-item");
+      }
+    });
+  });
+});
