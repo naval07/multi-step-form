d701984d80382d459ad669331444758b2529da07
Emanuel <emanuelnaval07@gmail.com>
2024-03-19 14:07:54 -0500

    Add - Added more functionality to the buttons

--- script.js
+++ script.js
@@ -1,92 +1,91 @@
-// const nextBtn = document.getElementById("next-btn");
-// const prevBtn = document.getElementById("prev-btn");
-
-// const form1 = document.querySelector(".element-1");
-// const form2 = document.querySelector(".element-2");
-
-// nextBtn.onclick = function () {
-//   form1.classList.remove("displayed-form");
-//   form1.classList.add("completed-form");
-
-//   form2.classList.remove("waiting-form");
-//   form2.classList.add("displayed-form");
-// };
-
-// prevBtn.onclick = function () {
-//   form2.classList.remove("displayed-form");
-//   form2.classList.add("waiting-form");
-
-//   form1.classList.remove("completed-form");
-//   form1.classList.add("displayed-form");
-// };
-
-// Setting the function of the next step and go back buttons
 const nextBtn = document.getElementById("next-step-btn");
 const goBackBtn = document.getElementById("go-back-btn");
+const stepNumbers = document.querySelectorAll(".step-number");
 let activeStepNumber = 0;
 
+// Setting the function of the next step button
 nextBtn.onclick = function () {
   // Check if the first form is displayed
-  if (activeStepNumber === 0) {
-    let currentForm = document.querySelector(".displayed-form");
-    let nextForm = document.querySelector(".waiting-form");
-    const stepNumbers = document.querySelectorAll(".step-number");
-    if (validateStep1()) {
-      goBackBtn.style.display = "inline-block";
-      currentForm.classList.remove("displayed-form");
-      currentForm.classList.add("completed-form");
-
-      nextForm.classList.remove("waiting-form");
-      nextForm.classList.add("displayed-form");
-
-      stepNumbers[activeStepNumber].classList.remove("step-number-active");
-      activeStepNumber += 1;
-      stepNumbers[activeStepNumber].classList.add("step-number-active");
-    }
-  }
-  // If true
-  // validate first form
-  // unhide the go back function
-  // show next form
-  // update the form step state bar
-  // If false
-  // validate current form
-  // show next form
-  // update the form step state bar
-};
-
-goBackBtn.onclick = function () {
   let currentForm = document.querySelector(".displayed-form");
   let nextForm = document.querySelector(".waiting-form");
+  if (activeStepNumber === 0) {
+    let validated = validateStep1();
+    if (validated) {
+      goBackBtn.style.display = "inline-block";
+      displayNextForm(currentForm, nextForm);
+    }
+  // Check if second form is displayed
+  } else if (activeStepNumber === 1) {
+    let validated = validateStep2();
+    if (validated) {
+      displayNextForm(currentForm, nextForm);
+    }
+  // Check if third form is displayed
+  } else if (activeStepNumber === 2) {
+    // Third form does not need to be validated
+    displayNextForm(currentForm, nextForm);
+    // Toggle next step button to confirm button
+    toggleNextStepBtn();
+  } 
+  // else if (activeStepNumber == 3) {
+  //   // confirm the entire form
+  // }
+};
+
+// Helper function to display next form
+function displayNextForm(current, next) {
+  // Set the current form to completed form
+  current.classList.remove("displayed-form");
+  current.classList.add("completed-form");
+  // Display the next form
+  next.classList.remove("waiting-form");
+  next.classList.add("displayed-form");
+  // Update the form step bar
+  stepNumbers[activeStepNumber].classList.remove("step-number-active");
+  activeStepNumber += 1;
+  stepNumbers[activeStepNumber].classList.add("step-number-active");
+}
+
+// Helper function to toggle next step button and confirm button
+function toggleNextStepBtn() {
+  nextBtn.classList.toggle('confirm-btn');
+  if (nextBtn.classList.contains('confirm-btn')) {
+    nextBtn.innerText = 'Confirm';
+  } else {
+    nextBtn.innerText = 'Next Step';
+  }
+}
+
+// Setting the function of the go back button
+goBackBtn.onclick = function () {
+  let currentForm = document.querySelector(".displayed-form");
   const stepNumbers = document.querySelectorAll(".step-number");
   // check if step number is > 1
   if (activeStepNumber > 0) {
+    // Remove current filled form information
     currentForm.reset();
+    // Hide current form
     currentForm.classList.remove("displayed-form");
     currentForm.classList.add("waiting-form");
-
+    // Display previous step form
     const completedForms = document.querySelectorAll(".completed-form");
     let previousForm = completedForms[completedForms.length - 1];
     previousForm.classList.remove("completed-form");
     previousForm.classList.add("displayed-form");
-
+    // Update the form step bar
     stepNumbers[activeStepNumber].classList.remove("step-number-active");
     activeStepNumber -= 1;
     stepNumbers[activeStepNumber].classList.add("step-number-active");
-
+    // Conditional changing features
     if (activeStepNumber === 0) {
+      // Hide go back button if went back to first form
       goBackBtn.style.display = "none";
+    } else if (activeStepNumber === 2) {
+      // Toggle next step button to confirm button on last form
+      toggleNextStepBtn();
     }
   }
-  // if True
-  // reset and hide current form
-  // display the last completed form
-  // update the form step state bar
 };
-
-function validateStep1() {
-  return true;
-}
 
 // FORM 1
 
@@ -94,6 +93,9 @@ function validateStep1() {
 // - Error messages
 // - Form button function
 // - change form state and function for previous form
+function validateStep1() {
+  return true;
+}
 
 // FORM 2
 // - Function for select only one plan at a time
@@ -101,6 +103,10 @@ function validateStep1() {
 // - Function that changes the time lapse plan
 // - Form buttons functions
 // - change form state and else
+
+function validateStep2() {
+  return true;
+}
 
 // Function that changes the layout of the selected label
 document.addEventListener("DOMContentLoaded", function () {
@@ -124,3 +130,26 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+// Function that toggles between monthly and Yearly plan
+const switchElement = document.querySelector('#switch-input');
+switchElement.onclick = function () {
+  // Set layout to active plan time text
+  const switchContainer = document.querySelector('.plan-switch-container');
+  switchContainer.childNodes.forEach((child) => {
+    if (child.tagName == 'P') {
+      child.classList.toggle("pricing-type-active");
+    }
+  })
+  // Toggling information of selected plan time
+  const monthlyPlanItems = document.querySelectorAll('.monthly-plan-item');
+  const yeralyPlanItems = document.querySelectorAll('.yearly-plan-item');
+  monthlyPlanItems.forEach((item) => item.classList.toggle('hide-plan-info')); yeralyPlanItems.forEach((item) => item.classList.toggle('hide-plan-info'));
+}
+
+// FORM 4
+const changeBtn = document.querySelector('#change-btn');
+changeBtn.onclick = function () {
+  goBackBtn.click();
+  goBackBtn.click();
+}
